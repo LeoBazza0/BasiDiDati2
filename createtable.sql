625890d613a5049b7db7bb428c716626d588d98f
LeoBazza0 <leonardo.baza@gmail.com>
2025-06-19 22:49:27 +0200

    second CREATE TABLE

--- createtable.sql
+++ createtable.sql
@@ -2,80 +2,66 @@
 cose da mettere eventualmente 
 CHECK (XXXXXX) 
 UNIQUE(XXXXXX) 
-Ospedale CHAR(5) NOT NULL REFERENCES Ospedale(Codice) ON UPDATE CASCADE 
-FOREIGN KEY(CF) REFERENCES Personale(CF) ON UPDATE CASCADE 
-PRIMARY KEY (XXX, XXX, XXX),
-
 */
 
 CREATE TABLE utente {
-Username CHAR(32) PRIMARY KEY,
-Email CHAR(64) NOT NULL,
-Password CHAR (64) NOT NULL,
-FotoProfilo IMAGE -- non so se va boolean o no -- se funziona il tipo image FOLLIA
+Username VARCHAR(32) PRIMARY KEY,
+Email VARCHAR(255) NOT NULL,
+Password VARCHAR (64) NOT NULL,
+FotoProfilo BYTEA, -- BYTEA in PostgreSQL, BLOB in MySQL oppure memorizzo il path a file salvati nel filesystem come VARCHAR -- se funziona il tipo image FOLLIA
 }
 
 CREATE TABLE moderatore{
-Username CHAR(32) PRIMARY KEY,
-Email CHAR(64) NOT NULL, -- serve anche questa?
+Username VARCHAR(32) PRIMARY KEY,
 DataNomina DATE NOT NULL, 
 FOREIGN KEY(Username) REFERENCES utente(Username) ON UPDATE CASCADE,
 }
 
 CREATE TABLE amministratore { 
-Username CHAR(32) PRIMARY KEY,
--- Email CHAR(64) NOT NULL, -- serve anche questa?
+Username VARCHAR(32) PRIMARY KEY,
 FOREIGN KEY(Username) REFERENCES utente(Username) ON UPDATE CASCADE,
 }
 
 CREATE TABLE post {
-IdPost CHAR (32) PRIMARY KEY
-Utente CHAR(64) NOT NULL REFERENCES utente(Username) ON UPDATE CASCADE,
+IdPost VARCHAR (32) PRIMARY KEY,
+Utente VARCHAR(64) NOT NULL REFERENCES utente(Username) ON UPDATE CASCADE,
 DataPubblicazione DATE NOT NULL,
 Moderato BOOLEAN NOT NULL,
 }
 
 CREATE TABLE immagine {
-IdPost CHAR (32) PRIMARY KEY,
-pic IMAGE, -- non funzionerà mai, ma l'idea è carina
+IdPost VARCHAR (32) PRIMARY KEY,
+pic BYTEA, -- BYTEA in PostgreSQL, BLOB in MySQL oppure memorizzo il path a file salvati nel filesystem come VARCHAR
 FOREIGN KEY(IdPost) REFERENCES post(IdPost) ON UPDATE CASCADE,
 }
 
 CREATE TABLE testo {
-IdPost CHAR (32) PRIMARY KEY,
+IdPost VARCHAR (32) PRIMARY KEY,
 txt VARCHAR(280),
 FOREIGN KEY(IdPost) REFERENCES post(IdPost) ON UPDATE CASCADE,
 }
 
 CREATE TABLE like {
-Utente CHAR (64) NOT NULL REFERENCES utente(Username) ON UPDATE CASCADE,
-Post CHAR (32) NOT NULL REFERENCES post(IdPost) ON UPDATE CASCADE,
-PRIMARY KEY (Utente, Post), -- serve?
+Utente VARCHAR (64) NOT NULL REFERENCES utente(Username) ON UPDATE CASCADE,
+Post VARCHAR (32) NOT NULL REFERENCES post(IdPost) ON UPDATE CASCADE,
+PRIMARY KEY (Utente, Post),
 }
 
 CREATE TABLE follow { 
-Utente1 CHAR (64) NOT NULL REFERENCES utente(Username) ON UPDATE CASCADE,
-Utente2 CHAR (64) NOT NULL REFERENCES utente(Username) ON UPDATE CASCADE,
-PRIMARY KEY (Utente1, Utente2), -- serve?
-}
-
--- non so se ha senso avere questa
-CREATE TABLE pubblica { 
-Utente CHAR (64) NOT NULL REFERENCES utente(Username) ON UPDATE CASCADE,
-Post CHAR (32) NOT NULL REFERENCES post(IdPost) ON UPDATE CASCADE,
-PRIMARY KEY (Utente, Post), -- serve?
+Utente1 VARCHAR (64) NOT NULL REFERENCES utente(Username) ON UPDATE CASCADE,
+Utente2 VARCHAR (64) NOT NULL REFERENCES utente(Username) ON UPDATE CASCADE,
+PRIMARY KEY (Utente1, Utente2), 
 }
 
 -- non so se ha senso avere questa
 CREATE TABLE moderare {
-Utente CHAR (64) NOT NULL REFERENCES moderatore(Username) ON UPDATE CASCADE,
-Post CHAR (32) NOT NULL REFERENCES post(IdPost) ON UPDATE CASCADE,
-PRIMARY KEY (Utente, Post), -- serve?
+Utente VARCHAR (64) NOT NULL REFERENCES moderatore(Username) ON UPDATE CASCADE,
+Post VARCHAR (32) NOT NULL REFERENCES post(IdPost) ON UPDATE CASCADE,
+PRIMARY KEY (Utente, Post),
 }
 
--- non so se ha senso avere questa
 CREATE TABLE flaggare {
-Utente CHAR (64) NOT NULL REFERENCES utente(Username) ON UPDATE CASCADE,
-Post CHAR (32) NOT NULL REFERENCES post(IdPost) ON UPDATE CASCADE,
-PRIMARY KEY (Utente, Post), -- serve?
+Utente VARCHAR (64) NOT NULL REFERENCES utente(Username) ON UPDATE CASCADE,
+Post VARCHAR (32) NOT NULL REFERENCES post(IdPost) ON UPDATE CASCADE,
+PRIMARY KEY (Utente, Post),
 }
